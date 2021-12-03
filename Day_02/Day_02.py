cf40bb8c67f53f0ae9c6869202bce7b42adce199
Daniel Holmes <DanielJHolmes@hotmail.co.uk>
2021-12-03 10:56:21 +0000

    Day_02 - Refactor - Python
    Replaced multiple if-else statements with dictionaries of lambda functions

--- Day_02/Day_02.py
+++ Day_02/Day_02.py
@@ -1,49 +1,48 @@
 from aoc_tools import Advent_Timer
 
 
+MOVE_DEFINITIONS_1 = {
+    "forward": (lambda pos, dist: Position(pos.x + dist, pos.depth)),
+    "down": (lambda pos, dist: Position(pos.x, pos.depth + dist)),
+    "up": (lambda pos, dist: Position(pos.x, pos.depth - dist))
+}
+
+
+MOVE_DEFINITIONS_2 = {
+    "forward": (lambda pos, dist: Position(pos.x + dist,
+                                           pos.depth + pos.aim*dist,
+                                           pos.aim)),
+    "down": (lambda pos, dist: Position(pos.x, pos.depth, pos.aim + dist)),
+    "up": (lambda pos, dist: Position(pos.x, pos.depth, pos.aim - dist))
+}
+
+
+class Position:
+    def __init__(self, x=0, depth=0, aim=0):
+        self.x = x
+        self.depth = depth
+        self.aim = aim
+
+
 def read_input(filename="input.txt"):
     with open(filename, "r") as file:
         data = [line.strip().split() for line in file]
     return data
 
 
-def make_move_1(move, depth, position, aim):
+def make_move(move, position, move_definitions):
     direction, distance = move
 
-    if direction == "forward":
-        position += int(distance)
-    elif direction == "down":
-        depth += int(distance)
-    elif direction == "up":
-        depth -= int(distance)
-
-    return depth, position, aim
+    return move_definitions[direction](position, int(distance))
 
 
-def make_move_2(move, depth, position, aim):
-    direction, distance = move
-
-    if direction == "forward":
-        dist = int(distance)
-        position += dist
-        depth += aim*dist
-    elif direction == "down":
-        aim += int(distance)
-    elif direction == "up":
-        aim -= int(distance)
-
-    return depth, position, aim
-
-
-def make_all_moves(move_set, move_def):
-    position = 0
-    depth = 0
-    aim = 0
+def make_all_moves(move_set, move_definitions):
+    position = Position()
 
     for move in move_set:
-        depth, position, aim = move_def(move, depth, position, aim)
+        position = make_move(move, position, move_definitions)
 
-    return depth, position
+    return position
 
 
 if __name__ == "__main__":
@@ -55,13 +54,13 @@ if __name__ == "__main__":
     timer.checkpoint_hit()
 
     # star 1
-    depth, position = make_all_moves(data, make_move_1)
-    print("Star 1: {}".format(depth*position))
+    position_1 = make_all_moves(data, MOVE_DEFINITIONS_1)
+    print("Star 1: {}".format(position_1.x * position_1.depth))
     timer.checkpoint_hit()
 
     # star 2
-    depth, position = make_all_moves(data, make_move_2)
-    print("Star 1: {}".format(depth*position))
+    position_2 = make_all_moves(data, MOVE_DEFINITIONS_2)
+    print("Star 2: {}".format(position_2.x * position_2.depth))
     timer.checkpoint_hit()
 
     timer.end_hit()
