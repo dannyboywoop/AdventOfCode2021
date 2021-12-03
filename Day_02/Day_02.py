9d5a33ea4618d3891b2e6c1af625dbbc0d999a07
Daniel Holmes <DanielJHolmes@hotmail.co.uk>
2021-12-03 10:06:32 +0000

    Day_02 - Star_02 - Python

--- Day_02/Day_02.py
+++ Day_02/Day_02.py
@@ -7,7 +7,7 @@ def read_input(filename="input.txt"):
     return data
 
 
-def make_move(move, depth, position):
+def make_move_1(move, depth, position, aim):
     direction, distance = move
 
     if direction == "forward":
@@ -17,15 +17,31 @@ def make_move(move, depth, position):
     elif direction == "up":
         depth -= int(distance)
 
-    return depth, position
+    return depth, position, aim
 
 
-def make_all_moves(move_set):
+def make_move_2(move, depth, position, aim):
+    direction, distance = move
+
+    if direction == "forward":
+        dist = int(distance)
+        position += dist
+        depth += aim*dist
+    elif direction == "down":
+        aim += int(distance)
+    elif direction == "up":
+        aim -= int(distance)
+
+    return depth, position, aim
+
+
+def make_all_moves(move_set, move_def):
     position = 0
     depth = 0
+    aim = 0
 
     for move in move_set:
-        depth, position = make_move(move, depth, position)
+        depth, position, aim = move_def(move, depth, position, aim)
 
     return depth, position
 
@@ -39,11 +55,13 @@ if __name__ == "__main__":
     timer.checkpoint_hit()
 
     # star 1
-    depth, position = make_all_moves(data)
+    depth, position = make_all_moves(data, make_move_1)
     print("Star 1: {}".format(depth*position))
     timer.checkpoint_hit()
 
     # star 2
+    depth, position = make_all_moves(data, make_move_2)
+    print("Star 1: {}".format(depth*position))
     timer.checkpoint_hit()
 
     timer.end_hit()
