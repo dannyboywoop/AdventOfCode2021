1797b64886522cdd6f54a42e6d8716a9982a10b1
Daniel Holmes <DanielJHolmes@hotmail.co.uk>
2021-12-04 11:54:52 +0000

    Day_04 - Star_02 - Python

--- Day_04/Day_04.py
+++ Day_04/Day_04.py
@@ -30,7 +30,7 @@ class BingoCard:
         col_win = True
         for i in range(len(self.grid)):
             col_win &= self.map[self.grid[i][col]][2]
-        
+
         return row_win or col_win
 
 
@@ -54,15 +54,34 @@ def find_winning_card(sequence, cards):
     return winner
 
 
-def star_1(sequence, cards):
-    winning_card, winning_num = find_winning_card(sequence, cards)
-    
+def calculate_score(winning_card, winning_num):
     total = 0
     for num, val in winning_card.map.items():
         if not val[2]:
             total += int(num)
-    
+
     return total * int(winning_num)
+
+
+def find_losing_card(sequence, cards):
+    remaining_cards = set(range(len(cards)))
+
+    # perform sequence
+    for num in sequence:
+        winning_card_nums = []
+
+        # mark remaining cards
+        for card_num in remaining_cards:
+            if cards[card_num].mark_card(num):
+                winning_card_nums.append(card_num)
+
+        # discard cards that have won
+        for card_num in winning_card_nums:
+            remaining_cards.remove(card_num)
+
+        # if this was the last card to win, return
+        if len(remaining_cards) == 0:
+            return cards[winning_card_nums[0]], num
 
 
 if __name__ == "__main__":
@@ -74,11 +93,16 @@ if __name__ == "__main__":
     timer.checkpoint_hit()
 
     # star 1
-    star_1_answer = star_1(sequence, cards)
+    winning_card, winning_num = find_winning_card(sequence, cards)
+    star_1_answer = calculate_score(winning_card, winning_num)
     print("Star_1: {}".format(star_1_answer))
     timer.checkpoint_hit()
 
     # star 2
+    remaining_sequence = sequence[sequence.index(winning_num)::]
+    losing_card, losing_num = find_losing_card(remaining_sequence, cards)
+    star_2_answer = calculate_score(losing_card, winning_num)
+    print("Star_2: {}".format(star_2_answer))
     timer.checkpoint_hit()
 
     timer.end_hit()
