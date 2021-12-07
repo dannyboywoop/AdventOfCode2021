d57015b63883d0bbeb28b68f2299ac6eed6b4241
Daniel Holmes <DanielJHolmes@hotmail.co.uk>
2021-12-07 12:37:03 +0000

    Day_07 - Star_02 - Python

--- Day_07/Day_07.py
+++ Day_07/Day_07.py
@@ -23,6 +23,39 @@ def best_spot_1(data):
         return fuel_cost_1(data, data[mid])
 
 
+def sum_natural_numbers(x):
+    return (x + 1) * x // 2
+
+
+def fuel_cost_2(data, point):
+    total = 0
+    for x in data:
+        total += sum_natural_numbers(abs(point-x))
+    return total
+
+
+def best_spot_2(data):
+    x = sum(data) // len(data)
+    x_fuel = fuel_cost_2(data, x)
+    min_found = False
+
+    while not min_found:
+        right_pos_fuel = fuel_cost_2(data, x + 1)
+        if right_pos_fuel < x_fuel:
+            x += 1
+            x_fuel = right_pos_fuel
+            continue
+
+        left_pos_fuel = fuel_cost_2(data, x - 1)
+        if left_pos_fuel < x_fuel:
+            x -= 1
+            x_fuel = left_pos_fuel
+            continue
+
+        break
+    return x_fuel
+
+
 if __name__ == "__main__":
     timer = Advent_Timer()
 
@@ -37,6 +70,8 @@ if __name__ == "__main__":
     timer.checkpoint_hit()
 
     # star 2
+    star_1_answer = best_spot_2(data)
+    print("Star 1: {}".format(star_1_answer))
     timer.checkpoint_hit()
 
     timer.end_hit()
